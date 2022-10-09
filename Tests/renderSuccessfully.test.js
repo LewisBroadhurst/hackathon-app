16854a4a1902e8e880a269ba3db20a375cc3b4ee
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-09 19:13:39 +0100

    Adding more tests

--- Tests/renderSuccessfully.test.js
+++ Tests/renderSuccessfully.test.js
@@ -3,6 +3,9 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Header from '../src/Components/Header/Header';
 import EventTablet from '../src/Components/EventTablet/EventTablet';
+import AddVenue from '../src/Components/AddVenue/AddVenue'
+import AdminMembers from '../src/Components/AdminMembers/AdminMembers';
+import AdminVenues from '../src/Components/AdminVenues/AdminVenues';
 
 // 1. Renders the specified component
 // 2. Tries to find the text "string" on the screen
@@ -18,6 +21,27 @@ it("Header renders", () => {
 it("EventTablet renders", () => {
     render(<EventTablet />);
     const linkElement = screen.getByText(/Upcoming/i)
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+it("AddVenue renders", () => {
+    render(<AddVenue />);
+    const linkElement = screen.getByText(/Register Venue/i)
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+it("AdminMembers renders", () => {
+    render(<AdminMembers />);
+    const linkElement = screen.getByText(/Active Members/i)
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+it("AdminVenues renders", () => {
+    render(<AdminVenues />);
+    const linkElement = screen.getByText(/Active Venues/i)
     
     expect(linkElement).toBeInTheDocument();
 });
