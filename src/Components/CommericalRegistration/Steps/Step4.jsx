0363dfc695297b4ba7b122c6bd6f10c9ea3cb43c
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-10-19 16:09:06 +0100

    .env variables working

--- src/Components/CommericalRegistration/Steps/Step4.jsx
+++ src/Components/CommericalRegistration/Steps/Step4.jsx
@@ -1,3 +1,4 @@
+import { CardElement, CardNumberElement, PaymentElement } from '@stripe/react-stripe-js';
 import React from 'react'
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
@@ -6,12 +7,17 @@ import { CommercialRegistrationContext } from '../../../Contexts/CommericalRegis
 const Step4 = () => {
 
   const {handleStepBackward} = useContext(CommercialRegistrationContext);
+  console.log(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+  console.log(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
   return (
     <section>
         <h2>Welcome to the platform!</h2>
 
-        
+        <form>
+          <CardElement />
+        </form>
+
         <button className='btn' onClick={handleStepBackward}>Back</button>
         <button className='btn'><Link to={'/groupOverview'} >Lets go!</Link></button>
     </section>
--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { CommercialRegistrationProvider } from './Contexts/CommericalRegistration.context';
-
-
+import {Elements} from '@stripe/react-stripe-js';
+import {stripePromise} from './Stripe/Stripe.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <CommercialRegistrationProvider>
-
+  <Elements stripe={stripePromise}>
     <App />
-
+  </Elements>
   </CommercialRegistrationProvider>
   </React.StrictMode>
 );
